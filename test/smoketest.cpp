4a029fddd7252b6de26c392009638b4eadab4b93
Gor Nishanov <GorNishanov@gmail.com>
2023-06-11 20:12:52 +0300

    first pass at pop push

--- test/smoketest.cpp
+++ test/smoketest.cpp
@@ -7,5 +7,9 @@ using namespace std::experimental;
 
 TEST_CASE("smoketest", "[smoketest]") {
   buffer_queue<int> q(10);
-  REQUIRE(1 == 1);
+  q.push(1);
+  q.push(2);
+
+  REQUIRE(q.pop() == 1);
+  REQUIRE(q.pop() == 2);
 }
