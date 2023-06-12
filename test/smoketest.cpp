cc4eb2321749282c0534af69764b2ebb94839dc2
Gor Nishanov <GorNishanov@gmail.com>
2023-06-12 13:58:47 +0300

    finally make sender works

--- test/smoketest.cpp
+++ test/smoketest.cpp
@@ -1,15 +1,37 @@
+#include <std/experimental/conqueue>
+
 #include <catch2/catch_test_macros.hpp>
 
-#include <std/experimental/conqueue>
+#include <exec/async_scope.hpp>
+#include <exec/static_thread_pool.hpp>
+#include <exec/task.hpp>
 
 using namespace std;
 using namespace std::experimental;
 
 TEST_CASE("smoketest", "[smoketest]") {
-  buffer_queue<int> q(10);
+  buffer_queue<int> q(2);
   q.push(1);
   q.push(2);
 
   REQUIRE(q.pop() == 1);
   REQUIRE(q.pop() == 2);
+}
+
+exec::task<void> coro(buffer_queue<int> &q) {
+  co_await q.async_push(3);
+  co_await q.async_push(4);
+}
+
+TEST_CASE("coro", "[coro]") {
+  exec::static_thread_pool pool(1);
+  exec::async_scope scope;
+  buffer_queue<int> q(2);
+  q.push(1);
+  q.push(2);
+
+  REQUIRE(q.pop() == 1);
+  REQUIRE(q.pop() == 2);
+  REQUIRE(q.pop() == 3);
+  REQUIRE(q.pop() == 4);
 }
