9c145561073dee9ff6f8ce00c09ae1aeab9eb1b7
Gor Nishanov <GorNishanov@gmail.com>
2023-06-12 18:13:05 +0300

    added async_pop

--- test/smoketest.cpp
+++ test/smoketest.cpp
@@ -11,6 +11,8 @@
 using namespace std;
 using namespace std::experimental;
 
+using stdexec::on;
+
 TEST_CASE("smoketest", "[smoketest]") {
   buffer_queue<int> q(2);
   q.push(1);
@@ -20,24 +22,46 @@ TEST_CASE("smoketest", "[smoketest]") {
   REQUIRE(q.pop() == 2);
 }
 
-exec::task<void> coro(buffer_queue<int> &q) {
+exec::task<void> coro_push(buffer_queue<int> &q) {
   co_await q.async_push(3);
   co_await q.async_push(4);
 }
 
-TEST_CASE("coro", "[coro]") {
+TEST_CASE("coro_push") {
   exec::static_thread_pool pool(1);
   exec::async_scope scope;
   buffer_queue<int> q(2);
   q.push(1);
   q.push(2);
 
-  scope.spawn(stdexec::on(pool.get_scheduler(), coro(q)));
+  scope.spawn(stdexec::on(pool.get_scheduler(), coro_push(q)));
 
   REQUIRE(q.pop() == 1);
   REQUIRE(q.pop() == 2);
   REQUIRE(q.pop() == 3);
   REQUIRE(q.pop() == 4);
+
+  stdexec::sync_wait(scope.on_empty());
+}
+
+exec::task<void> coro_pop(buffer_queue<int> &q) {
+  REQUIRE(co_await q.async_pop() == 1);
+  REQUIRE(co_await q.async_pop() == 2);
+  REQUIRE(co_await q.async_pop() == 3);
+  REQUIRE(co_await q.async_pop() == 4);
+}
+
+TEST_CASE("coro_pop") {
+  exec::static_thread_pool pool(1);
+  exec::async_scope scope;
+  buffer_queue<int> q(2);
+
+  scope.spawn(on(pool.get_scheduler(), coro_pop(q)));
+
+  q.push(1);
+  q.push(2);
+  q.push(3);
+  q.push(4);
 
   stdexec::sync_wait(scope.on_empty());
 }
