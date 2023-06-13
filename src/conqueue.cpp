e9e2bcf6582d39a6e275422f61e5da567fab67a7
Gor Nishanov <GorNishanov@gmail.com>
2023-06-13 15:19:54 +0300

    fix pointer alignment and typos

--- src/conqueue.cpp
+++ src/conqueue.cpp
@@ -6,7 +6,7 @@
 
 namespace std::experimental {
 
-static const char *get_string(conqueue_errc errc) {
+static const char* get_string(conqueue_errc errc) {
   switch (errc) {
   case conqueue_errc::success:
     return "success";
@@ -23,18 +23,18 @@ static const char *get_string(conqueue_errc errc) {
 
 struct __conqueue_error_category : std::error_category {
 public:
-  const char *name() const noexcept override { return "conqueue"; };
+  const char* name() const noexcept override { return "conqueue"; };
   string message(int ev) const override {
     return get_string(static_cast<conqueue_errc>(ev));
   };
 };
 
-const error_category &conqueue_category() noexcept {
+const error_category& conqueue_category() noexcept {
   static __conqueue_error_category cat;
   return cat;
 }
 
-conqueue_error::conqueue_error(const std::error_code &ec)
+conqueue_error::conqueue_error(const std::error_code& ec)
     : runtime_error(get_string(static_cast<conqueue_errc>(ec.value()))),
       ec(ec) {}
 
